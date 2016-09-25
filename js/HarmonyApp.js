f9ca4d7cab670994ed73f9025b0ed124bb67b00c
alanpm <alanpearsonmathews@gmail.com>
2016-09-25 15:57:57 -0700

    Added tracking view with a scrolling control

--- js/HarmonyApp.js
+++ js/HarmonyApp.js
@@ -5,38 +5,64 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 export default class HarmonyApp extends Component {
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.contentContainer} horizontal={true}>
-        <View style={styles.contentContainer}>
-          <Text>Soprano</Text>
-          <Text>Alto</Text>
-          <Text>Tenor</Text>
-          <Text>Bass</Text>
-        </View>
-      </ScrollView>
+      <TrackScrollingView />
     );
   }
 }
 
+const TrackScrollingView = () => {
+  var _scrollView: ScrollView;
+  return (
+    <View style={styles.window}>
+      <ScrollView
+        horizontal={true}
+        ref={(scrollView) => { _scrollView = scrollView; }}>
+        <View>
+          <TrackContainer color='yellow' />
+          <TrackContainer color='green' />
+          <TrackContainer color='blue' />
+          <TrackContainer color='red' />
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => { _scrollView.scrollTo({x: 300}); }}>
+        <Text>Scroll to start</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const TrackContainer = ({color}) => (
+  <View style={styles.trackContainer} backgroundColor={color}>
+    <Text>Harro</Text>
+  </View>
+)
+
 const styles = StyleSheet.create({
-  contentContainer: {
+  window: {
+    flex: 1,
+    marginTop: 200,
+    marginBottom: 200,
+  },
+  trackContainer: {
+    alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    width: 1000
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  button: {
+   margin: 7,
+   padding: 5,
+   alignItems: 'center',
+   backgroundColor: '#eaeaea',
+   borderRadius: 3,
+ }
 });
